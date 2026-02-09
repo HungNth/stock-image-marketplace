682eec84d73f21ccd98e093cf00c482705e0e4cb
Hungnth <thienhungnth@gmail.com>
2026-02-09 16:57:12 +0700

    feat: add fillable properties and relationships to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['total', 'user_id', 'picture_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function picture(): BelongsTo
+    {
+        return $this->belongsTo(Picture::class);
+    }
 }
