58595041834e778d92afe7ccd9f23945851595c8
Hungnth <thienhungnth@gmail.com>
2026-02-09 16:57:22 +0700

    feat: add fillable properties, relationships, and accessor to Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    //
+    protected $fillable = ['comment', 'user_id', 'approved', 'rating', 'picture_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function picture(): BelongsTo
+    {
+        return $this->belongsTo(Picture::class);
+    }
+
+    public function getCreatedAtAttribute($value): string
+    {
+        return Carbon::parse($value)->diffForHumans();
+    }
 }
