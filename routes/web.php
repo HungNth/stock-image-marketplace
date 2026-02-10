7e0380775db5eeea5ca9aca7a197a4374400cd5f
Hungnth <thienhungnth@gmail.com>
2026-02-10 11:22:05 +0700

    feat: update routes to support admin authentication and dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('/auth', [AdminController::class, 'auth'])->name('admin.auth');
+
+Route::prefix('admin')->group(function () {
+    Route::middleware('admin')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.index');
+        Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout');
+    });
 });
