e31bdc1157862d11ef7d343915ed462cc69ee6d8
Hungnth <thienhungnth@gmail.com>
2026-02-10 16:18:24 +0700

    feat: add PictureController with endpoints for managing pictures and related data

---
+++ stock-image-backend/app/Http/Controllers/Api/PictureController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PictureResource;
+use App\Http\Resources\UserResource;
+use App\Models\Category;
+use App\Models\Picture;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PictureController extends Controller
+{
+    /**
+     * Fetch all pictures.
+     */
+    public function index()
+    {
+        return PictureResource::collection(Picture::whereStatus(1)->latest()->get());
+    }
+    
+    /**
+     * Fetch a picture by ID.
+     */
+    public function fetchById(Picture $picture)
+    {
+        if ( ! $picture->status) {
+            abort(404);
+        }
+        return PictureResource::make($picture);
+    }
+    
+    /**
+     * Fetch pictures by category.
+     */
+    public function fetchByCategory(Category $category)
+    {
+        
+        return PictureResource::collection($category->pictures);
+    }
+    
+    /**
+     * Fetch pictures by ext.
+     */
+    public function fetchByExt($ext)
+    {
+        $pictures = Picture::where('ext', $ext)->latest()->get();
+        return PictureResource::collection($pictures);
+    }
+    
+    /**
+     * Fetch pictures by title.
+     */
+    public function fetchByTerm(Request $request)
+    {
+        $searchTerm = $request->searchTerm;
+        $pictures = Picture::where('title', 'like', '%'.$searchTerm.'%')->latest()->get();
+        return PictureResource::collection($pictures);
+    }
+    
+    /**
+     * Fetch all the extensions.
+     */
+    public function fetchExtension()
+    {
+        $extension = Picture::select('ext')->distinct()->get();
+        return response()->json($extension);
+    }
+    
+    /**
+     * Upload files.
+     */
+    public function uploadFile(Request $request)
+    {
+        // get logged in user
+        $user = User::find($request->user_id);
+        // save file
+        $file = $request->file('file');
+        $file_name = $this->saveImage($file);
+        // save the data
+        Picture::create([
+            'title' => $request->title,
+            'price' => $request->price,
+            'user_id' => $request->user_id,
+            'category_id' => $request->category_id,
+            'file_path' => 'storage/user/images/'.$file_name,
+            'ext' => $file->getClientOriginalExtension(),
+        ]);
+        // return the response
+        return response()->json([
+            'message' => 'Picture has been saved successfully.',
+            'user' => UserResource::make($user),
+        ]);
+    }
+    
+    /**
+     * Save the picture in storage.
+     */
+    public function saveImage($file)
+    {
+        $file_name = time().'_'.'picture'.'_'.$file->getClientOriginalName();
+        $file->storeAs('user/images', $file_name, 'public');
+        
+        return $file_name;
+    }
+    
+    /**
+     * Download the picture from storage.
+     */
+    public function downloadPicture(Picture $picture)
+    {
+        return response()->download($picture->file_path);
+    }
+}
