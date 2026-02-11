09edaaf38baacb18004aedf494f9dbd6ad85de54
Hungnth <thienhungnth@gmail.com>
2026-02-11 09:34:40 +0700

    feat: add UserController with endpoints for user management (store, auth, logout)

---
+++ stock-image-backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Store new user.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        if ($request->validated()) {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'message' => 'Account created successfully',
+            ]);
+        }
+    }
+
+    /**
+     * Log in the user.
+     */
+    public function auth(AuthUserRequest $request)
+    {
+        if ($request->validated()) {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'error' => 'The credentials do not match any of our record.',
+                ]);
+            } else {
+                return response()->json([
+                    'user' => $user,
+                    'access_token' => $user->createToken('new_user')->plainTextToken,
+                ]);
+            }
+        }
+    }
+
+    /**
+     * Log out the user.
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
+}
