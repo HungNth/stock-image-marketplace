3f29eb83dbf62ad3010cc27861bbac4a7b4cce2d
Hungnth <thienhungnth@gmail.com>
2026-02-11 09:52:09 +0700

    feat: define and group API routes for user, category, order, picture, and review management

--- stock-image-backend/routes/api.php
+++ stock-image-backend/routes/api.php
@@ -1,8 +1,38 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\PictureController;
+use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', function (Request $request) {
+        return [
+            'user' => UserResource::make($request->user()),
+            'currentToken' => $request->bearerToken(),
+        ];
+    });
+
+    Route::post('user/logout', [UserController::class, 'logout']);
+    Route::post('store/picture', [PictureController::class, 'uploadFile']);
+    Route::post('store/order', [OrderController::class, 'store']);
+    Route::post('download/picture', [PictureController::class, 'downloadPicture']);
+    Route::post('store/review', [ReviewController::class, 'store']);
+    Route::put('update/review', [ReviewController::class, 'updateReview']);
+    Route::post('delete/review', [ReviewController::class, 'deleteReview']);
+});
+
+Route::post('user/register', [UserController::class, 'store']);
+Route::post('user/login', [UserController::class, 'auth']);
+Route::get('category/{hasPictures}', [CategoryController::class, 'index']);
+Route::get('pictures', [PictureController::class, 'index']);
+Route::get('pictures/{picture}', [PictureController::class, 'fetchById']);
+Route::get('pictures/category/{category}', [PictureController::class, 'fetchByCategory']);
+Route::get('pictures/extensions/{ext}', [PictureController::class, 'fetchByExt']);
+Route::post('find/pictures', [PictureController::class, 'fetchByTerm']);
+Route::get('extensions', [PictureController::class, 'fetchExtension']);
+
