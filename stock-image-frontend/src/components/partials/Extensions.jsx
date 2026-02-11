16b904a89910576303d7f8494995e5a27a53e6e6
Hungnth <thienhungnth@gmail.com>
2026-02-11 16:13:31 +0700

    feat: enhance Extensions component with dynamic selection and "All" reset functionality

--- stock-image-frontend/src/components/partials/Extensions.jsx
+++ stock-image-frontend/src/components/partials/Extensions.jsx
@@ -1,15 +1,37 @@
-export default function Extensions({ extensions }) {
+export default function Extensions({ extensions, pictureExt, setPictureExt, setCategoryId }) {
     return (
         <>
-            <h6>Extensions</h6>
+            <h6>
+                {
+                    pictureExt && <span
+                        style={{ cursor: 'pointer' }}
+                        className="text-capitalize text-danger fw-bold mx-1"
+                        onClick={() => {
+                            setCategoryId('');
+                            setPictureExt('');
+                        }}
+                    >All</span>
+                }
+                Extensions
+            </h6>
             <ul className="list-group">
                 {
-                    extensions?.map((extension, index) => (
+                    extensions?.map((item, index) => (
                         <li key={index} className="list-group-item border-0">
                             <div className="form-check flex-grow-1">
-                                <input type="radio" className="form-check-input" id={index} />
+                                <input type="radio"
+                                       className="form-check-input"
+                                       value={item.ext}
+                                       name="ext"
+                                       checked={item.ext == pictureExt}
+                                       onChange={(e) => {
+                                           setCategoryId('');
+                                           setPictureExt(e.target.value);
+                                       }}
+                                       id={index}
+                                />
                                 <label htmlFor={index} className="form-check-label">
-                                    {extension.ext}
+                                    {item.ext}
                                 </label>
                             </div>
                         </li>
