c2e789b0104ddd3aa26cd8e3469ae5a146f7c958
Hungnth <thienhungnth@gmail.com>
2026-02-12 15:19:04 +0700

    feat: add Picture component for displaying image details and add-to-cart functionality
    - Fetch and display picture details using Axios and Redux.
    - Handle loading states and errors gracefully.
    - Add add-to-cart functionality with Redux integration.

---
+++ stock-image-frontend/src/components/pictures/Picture.jsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../../helpers/config.js';
+import Spinner from '../layouts/Spinner.jsx';
+import { addToCart } from '../../redux/slices/cartSlice.js';
+
+export default function Picture() {
+    const [picture, setPicture] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const { user, token, isLoggedIn } = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    
+    useEffect(() => {
+        const fetchPictureById = async() => {
+            setLoading(true);
+            try {
+                const response = await axios.get(`${BASE_URL}/pictures/${id}`);
+                setPicture(response.data.data);
+                setLoading(false);
+            } catch (e) {
+                if (e?.response?.status === 404) {
+                    setError('The picture you are looking for does not exist.');
+                }
+                console.log(e);
+                setLoading(false);
+            }
+        };
+        fetchPictureById();
+    }, [id]);
+    
+    console.log(picture);
+    
+    return (
+        <div className="container">
+            {
+                loading ? (
+                    <Spinner />
+                ) : (
+                    error ? (
+                        <div className="row my-5">
+                            <div className="col-md-6 mx-auto">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <div className="alert-info alert my-3">
+                                            {error}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="row my-5">
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img
+                                            src={picture?.image_path}
+                                            alt={picture?.title}
+                                            className="img-fluid rounded"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <div className="card-header bg-white">
+                                        <div className="text-center mt-2 mb-4">
+                                            {picture?.title}
+                                        </div>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="d-flex justify-content-start align-items-center">
+                                                <img
+                                                    src={picture?.user.image_path}
+                                                    alt={picture?.user.name}
+                                                    className="img-fluid rounded-circle"
+                                                    height={60}
+                                                    width={60}
+                                                />
+                                                <div className="d-flex flex-column mx-2">
+                                                    <span className="fw-bold">{picture?.user.name}</span>
+                                                    <span className="text-muted">
+                                                        <i>
+                                                            {picture?.user.pictures.length}{' '}
+                                                            {picture?.user.pictures.length > 1 ? 'pictures' : 'picture'}
+                                                        </i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <span
+                                                    className="border border-dark p-2 fw-bold rounded shadow-sm">
+                                                    ${picture?.price}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="d-flex justify-content-center my-2">
+                                            <button className="btn btn-primary"
+                                                    onClick={() => dispatch(addToCart({
+                                                        id: picture?.id,
+                                                        image_path: picture?.image_path,
+                                                        title: picture?.title,
+                                                        price: picture?.price,
+                                                    }))}
+                                            >
+                                                <i className="bi bi-bag-plus"></i>{' '}Add to cart
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                )
+            }
+        </div>
+    );
+}
