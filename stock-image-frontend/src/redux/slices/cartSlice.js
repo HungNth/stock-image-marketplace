a6289cae86c0d6b2e7a03d0418065da70c2545fd
Hungnth <thienhungnth@gmail.com>
2026-02-12 15:19:33 +0700

    feat: add cartSlice with Redux Toolkit for cart state management
    - Implement actions for adding, removing, and clearing cart items.
    - Integrate Toast notifications for user feedback on cart actions.

---
+++ stock-image-frontend/src/redux/slices/cartSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+const initialState = {
+    cartItems: []
+};
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart(state, action) {
+            const item = action.payload;
+            const exists = state.cartItems.find(picture => picture.id === item.id);
+            // check if the picture already exists in the cart
+            // if exists, we tell the user that the picture is already in the cart
+            // if not, we add the picture to the cart
+            if (exists) {
+                toast.info('Picture already exists in your cart', {
+                    position: 'top-right',
+                });
+            } else {
+                state.cartItems = [item, ...state.cartItems];
+                toast.success('Picture added to the cart.', {
+                    position: 'top-right',
+                });
+            }
+        },
+        removeFromCart(state, action) {
+            const item = action.payload;
+            state.cartItems = state.cartItems.filter(picture => picture.id !== item.id);
+            toast.info('Picture removed from the cart.', {
+                position: 'top-right'
+            });
+        },
+        clearCartItems(state, action) {
+            state.cartItems = [];
+        }
+    }
+});
+
+const cartReducer = cartSlice.reducer;
+export const { addToCart, removeFromCart, clearCartItems } = cartSlice.actions;
+
+export default cartReducer;
