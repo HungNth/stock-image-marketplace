1e481b8b747fca1ba43c20e148f35f3c1fe50ff6
Hungnth <thienhungnth@gmail.com>
2026-02-12 10:40:22 +0700

    feat: enhance Header with user authentication and dynamic navigation
    - Add user authentication with token-based API integration.
    - Update navigation to show dynamic links based on login status.
    - Add logout functionality with Redux state updates and Toastify notifications.
    - Highlight active links using `useLocation`.

--- stock-image-frontend/src/components/layouts/Header.jsx
+++ stock-image-frontend/src/components/layouts/Header.jsx
@@ -1,6 +1,53 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL, getConfig } from '../../helpers/config.js';
+import { setCurrentUser, setLoggedInOut, setToken } from '../../redux/slices/userSlice.js';
+import { toast } from 'react-toastify';
 
 export default function Header() {
+    const { isLoggedIn, user, token } = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    useEffect(() => {
+        const getLoggedInUser = async() => {
+            try {
+                const response = await axios.get(`${BASE_URL}/user`,
+                    getConfig(token)
+                );
+                dispatch(setCurrentUser(response.data.user));
+            } catch (e) {
+                if (e?.response?.status === 401) {
+                    dispatch(setLoggedInOut(false));
+                    dispatch(setCurrentUser(null));
+                    dispatch(setToken(''));
+                }
+                console.log(e);
+            }
+        };
+        if (token) getLoggedInUser();
+    }, [dispatch, token]);
+    
+    const logoutUser = async() => {
+        try {
+            const response = await axios.post(`${BASE_URL}/user/logout`, null,
+                getConfig(token)
+            );
+            dispatch(setLoggedInOut(false));
+            dispatch(setCurrentUser(null));
+            dispatch(setToken(''));
+            toast.success(response.data.message, {
+                position: 'top-right',
+            });
+            navigate('/login');
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
@@ -13,29 +60,64 @@ export default function Header() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">
+                            <Link
+                                className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                                aria-current="page" to="/"
+                            >
                                 <i className="bi bi-house"></i>{' '}
                                 Home
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/upload">
+                            <Link
+                                className={`nav-link ${location.pathname === '/upload' ? 'active' : ''}`}
+                                to="/upload"
+                            >
                                 <i className="bi bi-upload"></i>{' '}
                                 Upload
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">
-                                <i className="bi bi-person-add"></i>{' '}
-                                Register
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">
-                                <i className="bi bi-person-fill-up"></i>{' '}
-                                Login
-                            </Link>
-                        </li>
+                        {
+                            isLoggedIn ? (
+                                <>
+                                    <li className="nav-item">
+                                        <Link
+                                            className={`nav-link ${location.pathname === '/profile' ? 'active' : ''}`}
+                                            to="/profile"
+                                        >
+                                            <i className="bi bi-person"></i>{' '}
+                                            {user?.name}
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <button className="nav-link border-0 bg-light" onClick={() => logoutUser()}>
+                                            <i className="bi bi-person-fill-down"></i>{' '}
+                                            Logout
+                                        </button>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="nav-item">
+                                        <Link
+                                            className={`nav-link ${location.pathname === '/register' ? 'active' : ''}`}
+                                            to="/register"
+                                        >
+                                            <i className="bi bi-person-add"></i>{' '}
+                                            Register
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link
+                                            className={`nav-link ${location.pathname === '/login' ? 'active' : ''}`}
+                                            to="/login">
+                                            <i className="bi bi-person-fill-up"></i>{' '}
+                                            Login
+                                        </Link>
+                                    </li>
+                                </>
+                            )
+                        }
                     </ul>
                 </div>
             </div>
